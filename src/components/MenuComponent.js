c9900c79169dd15bf875dd612b7b8d2ba74a779a
KVSarathkumar <kvsarathkumar1976@gmail.com>
2020-08-15 09:46:47 +0530

    assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
+import Dishdetail from '/home/sarath/Desktop/react/confusion/src/components/DishdetailComponent ';
 
 class Menu extends Component {
     constructor(props) {
@@ -13,22 +14,23 @@ class Menu extends Component {
       onDishSelect(dish) {
         this.setState({ selectedDish: dish});
     }
-    renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
-  }
+  //   renderDish(dish) {
+  //     if (dish != null)
+  //         return(
+            
+  //             <Card>
+  //                 <CardImg top src={dish.image} alt={dish.name} />
+  //                 <CardBody>
+  //                   <CardTitle>{dish.name}</CardTitle>
+  //                   <CardText>{dish.description}</CardText>
+  //                 </CardBody>
+  //             </Card>
+  //         );
+  //     else
+  //         return(
+  //             <div></div>
+  //         );
+  // }
             
   render() {
     const menu = this.props.dishes.map((dish) => {
@@ -43,17 +45,20 @@ class Menu extends Component {
             </Card>
           </div>
         );
-    });
+    })
     return (
       <div className="container">
           <div className="row">
               {menu}
           </div>
-          <div className="row">
-            <div  className="col-12 col-md-5 m-1">
+        
+                <Dishdetail dish={this.state.selectedDish}/>
+          
+            {/* <div  className="col-12 col-md-5 m-1"> 
               {this.renderDish(this.state.selectedDish)}
-            </div>
-          </div>
+             
+             </div> */}
+         
       </div>
   );
 }
